6f14944bcb505931b99ff78c3ae8e8928cd790f6
Juhász Miron <shiftylorenz@gmail.com>
2024-10-13 18:33:51 +0200

    IP typo

--- src/config/backend.js
+++ src/config/backend.js
@@ -2,7 +2,7 @@ import { useUserStore } from '@/store/user.js';
 import axios from 'axios';
 
 export const backend = {
-    url: 'https://51.89.92.1:8081'
+    url: 'https://51.89.92.138:8081'
 }
 
 export function sendLoggedInRequest(path, data)
